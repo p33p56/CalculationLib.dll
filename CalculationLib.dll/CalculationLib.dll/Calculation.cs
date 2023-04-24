497e68940bfb44465bfd3a0c54274c6a8ca475c2
p33p56 <131284757+p33p56@users.noreply.github.com>
2023-04-24 14:17:51 +0500

    Add files via upload

---
+++ CalculationLib.dll/CalculationLib.dll/Calculation.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace CalculationLib
+{
+    public class Calculation
+    {
+        public double Plus(double a, double b)
+        {
+            return a + b;
+        }
+
+        public double Minus(double a, double b)
+        {
+            return a - b;
+        }
+
+        public double Divide(double a, double b)
+        {
+            if (b == 0)
+            {
+
+            }
+            return a / b;
+        }
+
+        public double Multiply(double a, double b)
+        {
+            return a * b;
+        }
+    }
+}
---
+++ CalculationLib.dll/ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using CalculationLib;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculation calc = new Calculation();
+            double a = 25;
+            double b = 5;
+            Console.WriteLine("a = {0}", a);
+            Console.WriteLine("b = {0}", b);
+            Console.WriteLine("a + b = {0}", calc.Plus(a, b));
+            Console.WriteLine("a - b = {0}", calc.Minus(a, b));
+            Console.WriteLine("a / b = {0}", calc.Divide(a, b));
+            Console.WriteLine("a * b = {0}", calc.Multiply(a, b));
+            Console.ReadKey();
+        }
+    }
+}
